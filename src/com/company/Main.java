2725ab3e040e6425af1dbd33f17f9de9a7f60489
Jules <jules.duarte@edu.esiee-it.fr>
2021-10-06 12:19:06 +0200

    changement c

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,27 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
-    public static float nombreOperation;
+    public static float nombreOperation=0;
+    public static int comparaison =0;
+    public static int affectation = 0;
 
-    Random random = new Random();
+    //Random random = new Random();
 
     public static void stats (int min, int max, int step, int nbr) {
-       int i = 1;
+       int i;
        nombreOperation =0;
+       comparaison =0;
+       affectation =0;
         for (i = 1; i <= nbr ; i++){
             int T[] =new Random().ints(min).toArray();
             tri_insertion(T);
             }
             if (i>nbr && min<max){
-                System.out.println(min +" "+ nombreOperation/nbr);
+                System.out.println(min +" "+ nombreOperation/nbr +" le nombre de comparaisons en moyenne est de "+comparaison/nbr + ", le nombre d'affectations en moyenne est de " + affectation/nbr + " et le nombre d'échanges en moyenne est de "+ (affectation/3)/nbr +".");
                 stats((min+step),max,step,nbr);
         }
             else {
-                System.out.println(min +" "+ nombreOperation/nbr);
+                System.out.println(min +" "+ nombreOperation/nbr + " le nombre de comparaisons en moyenne est de "+comparaison/nbr + ", le nombre d'affectations en moyenne est de " + affectation/nbr + " et le nombre d'échanges en moyenne est de "+ (affectation/3)/nbr +".");
             }
     }
 
@@ -32,12 +36,16 @@ public static void tri_insertion (int T[]){
     for (int i =1; i< taille; i++){
         x = T[i];
         j = i-1;
+        affectation++;
         while (j>=0 && T[j]> x){
             T[j+1] = T[j];
             j--;
+            affectation++;
+            comparaison++;
             nombreOperation++;
         }
         T[j+1] =  x;
+        affectation++;
         nombreOperation++;
     }
 }
