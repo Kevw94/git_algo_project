2ead32d25f50d980bc57e6a7ac7ac728076b035d
Jules <jules.duarte@edu.esiee-it.fr>
2021-10-06 11:29:01 +0200

    update c

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,25 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
-    public static int nombreOperation =0;
+    public static float nombreOperation;
 
     Random random = new Random();
+
+    public static void stats (int min, int max, int step, int nbr) {
+       int i = 1;
+       nombreOperation =0;
+        for (i = 1; i <= nbr ; i++){
+            int T[] =new Random().ints(min).toArray();
+            tri_insertion(T);
+            }
+            if (i>nbr && min<max){
+                System.out.println(min +" "+ nombreOperation/nbr);
+                stats((min+step),max,step,nbr);
+        }
+            else {
+                System.out.println(min +" "+ nombreOperation/nbr);
+            }
+    }
 
 
 public static void tri_insertion (int T[]){
@@ -19,19 +35,18 @@ public static void tri_insertion (int T[]){
         while (j>=0 && T[j]> x){
             T[j+1] = T[j];
             j--;
-            System.out.println(Arrays.toString(T) + "w");
             nombreOperation++;
         }
         T[j+1] =  x;
-        System.out.println(Arrays.toString(T));
         nombreOperation++;
     }
 }
     public static void main(String[] args) {
 
-    int T[] =new Random().ints(10,0,10).toArray();
-
-    tri_insertion(T);
-        System.out.println(nombreOperation);
+    //int T[] =new Random().ints(10,0,10).toArray();
+    //int T[] = {1,2,3,4,5,6,7,8};
+    //tri_insertion(T);
+        //System.out.println(nombreOperation);
+        stats(10,20,5,10);
     }
 }
