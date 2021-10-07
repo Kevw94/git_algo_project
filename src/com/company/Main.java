6b011730fc21b41740d6950e05b753f450e6ab57
Kevw94 <77940775+Kevw94@users.noreply.github.com>
2021-10-07 10:22:29 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,51 +7,60 @@ import java.util.Random;
 public class Main {
 
     public static float nombreOperation = 0;
+//    public static int comparisonNumbers =
 
 
-     public static void soartedSelection(int[] array){
-            int i; // fist for in array
-            int attributedValues;
-            int j; // second for in array
-            int organisation;
-            for (i = 0; i <=  array.length -1 ; i++) {
-                attributedValues = i;
-                nombreOperation =nombreOperation + 1;
-                for (j = i + 1; j <=  array.length - 2 ; j++) {
-                    if (array[j] < array[attributedValues]) {// selection
-                        attributedValues = j;
-                        nombreOperation =nombreOperation + 1;// count
-                        organisation = array[attributedValues];
-                        array[attributedValues] = array[i];
-                        array[i] = organisation;
-                    }
+     public static void soartedSelection(int[] array) {
+         int i; // fist for in array
+         int min; // min of each array
+         int j; // second for in array
+         int organisation;
+         for (i = 0; i < array.length - 1; i++) {
+             min = i;
+             nombreOperation += 1; //count
+             for (j = i + 1; j < array.length; j++) {
+                 if (array[j] < array[min]) {// selection
+                     nombreOperation += 1; // count
+                     min = j;
+                     nombreOperation += 1;// count
+                     organisation = array[min];
+                     nombreOperation += 1;// count
+                     array[min] = array[i];
+                     nombreOperation += 1;// count
+                     array[i] = organisation;
+                     nombreOperation += 1;// count
+                 }
+             }
 
-
-
-
-                }
-            }
-        }
-
-
-    public static void stats (int min, int max, int step, int nbr) {
-        int i;
-        nombreOperation = 0;
-        for (i = 1; i <= nbr ; i++){
-            int T[] =new Random().ints(min).toArray();
-            soartedSelection(T);
-        }
-        if (i>nbr && min<max){
-            System.out.println(min +" "+ nombreOperation/nbr);
-            stats((min+step),max,step,nbr);
-        }
-        else {
-            System.out.println(min +" "+ nombreOperation/nbr);
-        }
-    }
+         }
 
 
 
+     }
+
+
+         public static void stats ( int min, int max, int step, int nbr){
+             int i;
+             nombreOperation = 0;
+             for (i = 1; i <= nbr; i++) {
+                int array[] = new Random().ints(min).toArray();
+                 soartedSelection(array);
+             }
+             if (i > nbr && min < max) {
+                 System.out.println(" Nombre d'opérations dans le pire des cas pour l'exemple  " + min*min*nbr + " " +  " Nombre d'opérations moyennes    "  + nbr * (min*(min-1))/2);
+                 stats((min + step), max, step, nbr);
+             } else {
+                 System.out.println(" Nombre d'opérations dans le pire des cas pour l'exemple  " + min*min*nbr + " " +  " Nombre d'opérations moyennes   "  + nbr * (min*(min-1))/2);
+             }
+         }
+
+         public static void printArray ( int array[])
+         {
+
+             for (int i = 0; i < array.length; ++i)
+                 System.out.print(array[i] + " ");
+             System.out.println();
+         }
 
 
 
@@ -69,9 +78,15 @@ public class Main {
 
 
     public static void main(String[] args) {
+        stats(10,1000,5,10);
 
 
-        stats (10, 20,5 , 10);
+
+        int[] array = {1,4,2,7,25,20,10};
+        soartedSelection(array);
+        printArray(array);
+
+
 
 
 
@@ -87,4 +102,3 @@ public class Main {
         // write your code here
     }
 }
-
